9228fab1363a67352fb1d29dbf92b303b156e47c
sagar aryal <sagar632aryal@gmail.com>
2020-01-23 15:34:10 +0545

    sagar6

--- app/src/main/java/com/example/bhramand123/signin.java
+++ app/src/main/java/com/example/bhramand123/signin.java
@@ -3,6 +3,7 @@ package com.example.bhramand123;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,13 +47,15 @@ private FirebaseAuth mauth;
         @Override
         public void onSuccess(AuthResult authResult) {
             Toast.makeText(getApplicationContext(),"Signed in successfully",Toast.LENGTH_SHORT).show();
-
+        startActivity(new Intent(getApplicationContext(),));
 
         }
     }).addOnFailureListener(new OnFailureListener() {
         @Override
         public void onFailure(@NonNull Exception e) {
             Toast.makeText(getApplicationContext(),"Sorry try again",Toast.LENGTH_SHORT).show();
+            memail.setText("");
+            mpassword.setText("");
 
         }
     });
--- app/src/main/java/com/example/bhramand123/signup.java
+++ app/src/main/java/com/example/bhramand123/signup.java
@@ -3,6 +3,7 @@ package com.example.bhramand123;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -73,12 +74,18 @@ mauth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new On
         mdbr.child(uiid).child("password").setValue(password);
         mdbr.child(uiid).child("phoneno").setValue(phoneno);
         Toast.makeText(getApplicationContext(),"Account created successfully",Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(getApplicationContext(),MainActivity.this));
+
 
     }
 }).addOnFailureListener(new OnFailureListener() {
     @Override
     public void onFailure(@NonNull Exception e) {
         Toast.makeText(getApplicationContext(),"Account not created",Toast.LENGTH_SHORT).show();
+        memail.setText("");
+        mname.setText("");
+        mphoneno.setText("");
+        mpassword.setText("");
     }
 });
     }
