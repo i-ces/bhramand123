b7cc44acb4a00c2d18a0d8bebe050452e55cbfe2
sandesh <sandesh0806@gmail.com>
2020-01-23 16:26:26 +0545

    fragment

--- app/src/main/java/com/example/bhramand123/HomeActivity.java
+++ app/src/main/java/com/example/bhramand123/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.bhramand123;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
