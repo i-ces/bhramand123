0f9279324a262b759081781d78baa4947803b6eb
sagar aryal <sagar632aryal@gmail.com>
2020-01-24 15:30:45 +0545

    Merge branch 'master' of https://github.com/sagar632/bhramand123
    # Conflicts:
    #	app/src/main/res/layout/activity_signup.xml

--- app/src/main/java/com/example/bhramand123/EventDetailsActivity.java
+++ app/src/main/java/com/example/bhramand123/EventDetailsActivity.java
@@ -2,13 +2,30 @@ package com.example.bhramand123;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class EventDetailsActivity extends AppCompatActivity {
+private ImageView eventflex;
+private TextView eventname;
+private TextView eventdesc;
+private TextView eventdate;
+private TextView locationlink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+        eventflex=findViewById(R.id.imageView5);
+        eventname=findViewById(R.id.textView6);
+        eventdesc=findViewById(R.id.textView10);
+        eventdate=findViewById(R.id.textView8);
+        locationlink=findViewById(R.id.textView9);
+
+
+
     }
 }
